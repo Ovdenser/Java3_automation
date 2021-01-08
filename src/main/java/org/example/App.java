c382cac500ef0b243572193db2d40c82671522bf
Ovdenser <ovdenser@gmail.com>
2021-01-08 20:09:43 +0300

    Homework2(4)

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,13 +1,44 @@
 package org.example;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+public class App {
+    public static WebDriver driver;
+
+    public static void main(String[] args) throws InterruptedException {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+
+        driver.get("https://.google.com");
+        driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+        WebElement input = driver.findElement(By.cssSelector("input[title='Поиск']"));
+        input.click();
+        input.sendKeys("github");
+        input.sendKeys(Keys.ENTER);
+
+//        String.wikiLink = "https://ru.wikipedia.org/wiki/GitHub";
+        WebElement wikiLink = driver.findElement(By.linkText("Википедия"));
+        new WebDriverWait(driver, 5).until(ExpectedConditions.elementToBeClickable(wikiLink));
+        String wikiLinkString = wikiLink.getCssValue("href");
+        wikiLink.click();
+        System.out.println(wikiLinkString);
+        List<WebElement> divElements = driver.findElements(By.tagName("shlyapa"));
+        if (divElements.size() == 0) {
+            driver.quit();
+            throw new NoSuchElementException("0 elements found");
+        }
+        System.out.println(divElements.size());
+        driver.quit();
     }
 }
+
+
+
--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -1,20 +1,41 @@
 package org.example;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.Test;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+
+public class AppTest {
+    public static WebDriver driver;
+    public final String WIKILINK = "https://ru.wikipedia.org/wiki/GitHub";
+
+
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void gitWikiLinkTest() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.get("https://.google.com");
+        driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+
+        WebElement input = driver.findElement(By.cssSelector("input[title='Поиск']"));
+        input.click();
+        input.sendKeys("github");
+        input.sendKeys(Keys.ENTER);
+
+        WebElement wikiLink = driver.findElement(By.linkText("Википедия"));
+        String wikiLinkString = wikiLink.getAttribute("href");
+
+        assertEquals(WIKILINK, wikiLinkString);
+        driver.quit();
+
     }
 }
